020fe14b072f91f176ed18a0e2cc62fd316cd083
edunetmastertrainer <mastertrainermern@gmail.com>
2025-12-03 12:59:50 +0530

    Add files via upload
    gg

---
+++ authdemo/useraccount/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from useraccount import views
+urlpatterns = [
+    path('registration/',views.registration,name='registration'),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/",views.dashboard,name='dashboard')
+]
